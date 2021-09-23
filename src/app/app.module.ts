699841326c0606093bdc14f0317cba2e24c93915
hasannasrul <hasanali242424@gmail.com>
2021-09-23 23:21:43 +0530

    6 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,12 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ServersComponent } from './servers/servers.component';
 import { UsersComponent } from './users/users.component';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, AppRoutingModule ],
   declarations: [ AppComponent,ServersComponent, UsersComponent],
   bootstrap:    [ AppComponent ]
 })
