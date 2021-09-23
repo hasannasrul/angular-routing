6f6ca3ee3a13fe82f7b052748231af474cf27c63
hasannasrul <hasanali242424@gmail.com>
2021-09-23 22:22:40 +0530

    4 files modified, 6 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { UsersComponent } from './users/users.component';
+import { ServersComponent } from './servers/servers.component';
 
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, HomeComponent, UsersComponent, ServersComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
